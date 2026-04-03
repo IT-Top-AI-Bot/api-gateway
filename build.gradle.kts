b13e6a608415dc0515ffbf42945452e6ccdc5f5e
Ворончихин Иван <aqua.dev@yandex.com>
2026-04-03 12:41:29 +0300

    Downgrades gRPC Netty to 1.70.0 and adds explicit dependency declaration.

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,7 @@ repositories {
 }
 
 extra["springCloudVersion"] = "2025.1.1"
+val grpcNettyVersion by extra("1.70.0")
 val opentelemetryVersion by extra("2.21.0-alpha")
 
 dependencies {
@@ -28,7 +29,7 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-config")
     implementation("org.springframework.cloud:spring-cloud-starter-gateway-server-webflux")
     implementation("org.springframework.cloud:spring-cloud-starter-kubernetes-client-all")
-
+    implementation("io.grpc:grpc-netty:$grpcNettyVersion")
 }
 
 dependencyManagement {
